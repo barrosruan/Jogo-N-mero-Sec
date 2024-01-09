ce41b667cb4e75d1ba741a7c4b229f0bac0d1531
Ruan Silva <ruan.fox.2013@gmail.com>
2024-01-09 16:33:27 -0300

    implementando funções

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let = tentativas = 1;
 
 function exibirtextoNaTela(tag, texto) {
   let campo = document.querySelector(tag);
@@ -9,7 +10,20 @@ exibirtextoNaTela("h1", "Jogo do número secreto");
 exibirtextoNaTela("p", "Escolha um número entre 1 e 10");
 
 function verificarChute() {
-  console.log(numeroSecreto);
+  let chute = document.querySelector("input").value;
+  if (chute == numeroSecreto) {
+    exibirtextoNaTela("h1", "Acertou!");
+    let palavraTentativas = tentativas > 1 ? "tentativas" : "tentativa";
+    let mensagemtentativas = ` Você descobriu o número secreto com ${tentativas} ${palavraTentativas}!`;
+    exibirtextoNaTela("p", mensagemtentativas);
+  } else {
+    if (chute > numeroSecreto) {
+      exibirtextoNaTela("p", "O número secreto é menor");
+    } else {
+      exibirtextoNaTela("p", "O número secreto é maior");
+    }
+    tentativas++;
+  }
 }
 
 function gerarNumeroAleatorio() {
